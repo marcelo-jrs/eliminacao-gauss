19026f2e324684ecdcb3f6eb7cbf066ab0abd6d4
Marcelo <fmarcelocarlos@gmail.com>
2023-06-05 21:40:13 -0300

    eliminação de gauss concluida

--- gauss.py
+++ gauss.py
@@ -1,26 +1,63 @@
-def eliminacao_gauss(matriz):
-    linhas = len(matriz)
-    colunas = len(matriz[0])
+import numpy as np
 
-    for i in range(linhas):
-        # encontra o pivo principal
-        pivot = matriz[i][i]
-
-        # se o pivo for zero troca a linha
-        if pivot == 0:
-            for j in range(i+1, linhas):
-                if matriz[j][i] != 0:
-                    matriz[i], matriz[j] = matriz[j], matriz[i]
-                    pivot = matriz[i][i]
+def encontrar_pivo(matriz):
+    nlinhas, ncolunas = matriz.shape
+    
+    for i in range(nlinhas):
+        pivo = None
+        
+        # Procurar o pivô não nulo na linha atual
+        for j in range(ncolunas):
+            if matriz[i, j] != 0:
+                pivo = (i, j)
+                break
+        
+        # Se não encontrou um pivô não nulo, procurar nas linhas abaixo
+        if pivo is None:
+            for k in range(i+1, nlinhas):
+                if matriz[k, i] != 0:
+                    pivo = (k, i)
+                    matriz[[i, k]] = matriz[[k, i]]  # Trocar as linhas i e k
                     break
+        
+        if pivo is not None:
+            return pivo
+    
+    return None
 
-        # Se não houver pivo retorna none
-        if pivot == 0:
-            return None
+def eliminacao_gauss(matriz):
+    nlinhas, ncolunas = matriz.shape
+    
+    for i in range(nlinhas):
+        # Encontrar o pivô
+        pivo =  encontrar_pivo(matriz[i:, :])
+        
+        if pivo is None:
+        # Se não houver pivô, a eliminação está completa
+            break
+    
+        linha_pivo, coluna_pivo = pivo
+        
+        linha_pivo += i  # Ajustar o índice da linha do pivô
+        
+        # Dividir a linha do pivô pelo valor do pivô para torná-lo igual a 1
+        pivô = matriz[linha_pivo, coluna_pivo]
+        matriz[linha_pivo, :] = np.divide(matriz[linha_pivo, :], pivô)
 
-        # multiplica a linha do pivo para eliminar os elementos abaixo dele
-        for j in range(i+1, linhas):
-            factor = matriz[j][i] / pivot
-            matriz[j] = [elem - factor * matriz[i][k] for k, elem in enumerate(matriz[j])]
-
+        
+        # Eliminação - multiplicar o valor do pivô pela linha atual e subtrair dessa linha
+        for k in range(linha_pivo + 1, nlinhas):
+            coeficiente = matriz[k, coluna_pivo]
+            matriz[k, :] -= coeficiente * matriz[linha_pivo, :]
+    
     return matriz
+
+# Exemplo de uso
+AB = np.array([[3, 2, -1, 4],
+ [2, -1, 3, 6],
+ [1, 3, -2, 5]])
+
+resultado = eliminacao_gauss(AB)
+
+print("Resultado da eliminação de Gauss:")
+print(resultado)
